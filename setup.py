821bc956ec27d867f937f8ec73a04420a738e2cb
J0RdN-w <yordanoswuletaw@gmail.com>
2024-01-07 13:39:11 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distributions',
+      version='0.1',
+      description='Probability Distributions',
+      packages=['prob_distributions'],
+      author = 'Yordanos Wuletaw',
+      author_email = 'yordanoswuletaw@gmail.com',
+      zip_safe=False)
